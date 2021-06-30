859558832fbabbcbf252f82005b70127bd628389
jackalwonder <63820064+jackalwonder@users.noreply.github.com>
2021-06-30 13:57:38 +0800

    nothing

---
+++ src/com/mj/Main.java
@@ -0,0 +1,10 @@
+package com.mj;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(11);
+        list.add(12);
+    }
+}
